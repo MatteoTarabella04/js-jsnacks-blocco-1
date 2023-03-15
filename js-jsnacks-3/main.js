e1c615df879cc21847d59ce4ac7a70970cd91049
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-15 12:07:22 +0100

    ex done

--- js-jsnacks-3/main.js
+++ js-jsnacks-3/main.js
@@ -5,18 +5,39 @@ Il programma stampa la somma di tutti i numeri inseriti.
 */
 
 // dichiaro una variabile array per i numeri
-numbers = [];
+const numbers = [];
 
 // avvio uun ciclo che richiede 10 numeri all'utente
-for (let i = 0; i < 10; i++) {
-   const input = Number(prompt('inserisci un numero'));
-   numbers.push(input);
+/* for (let i = 0; i < 10; i++) {
+    const input = Number(prompt('inserisci un numero'));
+    numbers.push(input);
+}
+ */
+/* let sum = 0 */
+
+/* for (i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
 }
 
-let sum = 0 
+console.log(sum); */
 
-for (i= 0; i < numbers.length; i++ ) {
-    sum += numbers [i]
+/*
+SNACK 3
+Rifare con WHILE LOOP:  Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. 
+*/
+
+let y = 0;
+let sum = 0;
+
+while (y < 10) {
+
+    const input = Number(prompt('inserisci un numero'));
+    
+    sum += numbers[y]; 
+    numbers.push(input)
+    
+    
+    y ++;
 }
-
-console.log(sum);
+console.log(Number(sum));
